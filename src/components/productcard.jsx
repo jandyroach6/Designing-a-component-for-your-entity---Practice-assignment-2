a2fe91050cceed740371ddb4171f9e0249974d70
unknown <jandyroachy@gmail.com>
2025-03-06 11:02:38 +0530

    completed

--- src/components/productcard.jsx
+++ src/components/productcard.jsx
@@ -1,1 +1,28 @@
 // write product card here
+import React from "react";
+import ViewProductButton from "../components/button";
+import "../index.css";
+
+const Productcard = () => {
+  // Static product details
+  const productImage = "./src/assets/fashion.webp";
+  const productName = "Anarkali Suit";
+  const price = "$49.99";
+
+  return (
+    <div>
+      <img 
+        id = "productImage"
+        className="dress"
+        src={productImage}
+      />
+      <h2>{productName}</h2>
+      <p>{price}</p>
+      <div>
+        <ViewProductButton />
+      </div>
+    </div>
+  );
+};
+
+export default Productcard;
