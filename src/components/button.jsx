9e02072a4e2a2a67a6d2f0ca206d4a0c28d899c2
unknown <jandyroachy@gmail.com>
2025-03-06 11:04:46 +0530

    completed

--- src/components/button.jsx
+++ src/components/button.jsx
@@ -3,7 +3,7 @@ import React from "react";
 
 const ViewProductButton = () => {
   return (
-    <button className="bg-blue-500 text-white px-4 py-2 rounded hover:bg-blue-600 transition">
+    <button>
       View Product
     </button>
   );
